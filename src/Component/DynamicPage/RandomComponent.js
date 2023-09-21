46e62c2ed17db263f61b01d1712aa62a18559620
rushikesh39 <123180691+rushikesh39@users.noreply.github.com>
2023-09-21 09:56:43 +0530

    Update RandomComponent.js
    Created by siddhesh

--- src/Component/DynamicPage/RandomComponent.js
+++ src/Component/DynamicPage/RandomComponent.js
@@ -49,3 +49,30 @@ function RandomComponent(props) {
 }
 
 export default RandomComponent;
+
+
+/*
+Siddhesh Logic 
+
+let arr = [2,4,6,4,3,8,6,3,2];
+let random_val = [];
+let use_index = [];
+
+for(let i = 0; random_val.length < 3; i++){
+    let random_num = Math.floor(Math.random() * arr.length);
+    
+    if(!use_index.includes(random_num)){
+        if(!random_val.includes(arr[random_num])){
+            random_val.push(arr[random_num]);
+            use_index.push(random_num);
+            
+            console.log("random value : "+random_val)
+            console.log("used index : "+use_index)
+        }
+    }
+}
+
+
+
+
+*/
