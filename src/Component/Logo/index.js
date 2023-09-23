b869200b6ec3c35d0fcd8920ae40105df4dd6f66
rushikesh39 <123180691+rushikesh39@users.noreply.github.com>
2023-09-23 18:54:03 +0530

    Update index.js

--- src/Component/Logo/index.js
+++ src/Component/Logo/index.js
@@ -3,14 +3,16 @@ import "./logo.style.css";
 import React, { useContext } from "react";
 // import { store } from '../../Store/StoreCompo';
 import { store } from "../../Store/StoreCompo";
+import { useNavigate } from "react-router-dom";
 
 const Logo = () => {
+  const navi=useNavigate()
   const data = useContext(store);
   // console.log("data", data.StoreData);
   const subheading = data.StoreData[0].headertxt;
   const logo = data.StoreData[0].logo;
   return (
-    <div className="logo">
+    <div className="logo pointer" onClick={()=>{navi("/")}}>
       <div className="subheading">{subheading}</div>
       <div><h1>{logo}</h1></div>
     </div>
